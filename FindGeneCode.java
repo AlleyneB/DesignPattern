989c34a3d4871bb6557ef6bdb4f61afa930ec911
AlleyneB <libo7989@gmail.com>
2017-12-05 20:01:24 +0800

    添加TreeMap保存匹配顺序

--- FindGeneCode.java
+++ FindGeneCode.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 public class FindGeneCode {
 	public static void main(String[] args){
 		String[] chosedGeneHead=new String[] {"GAG","AAA","AAT"};
 		File seqsFile=new File("C:\\Users\\Alleyne\\Documents\\javaStudy\\R.txt");
 		Map<String, String> seqsMap=getSeqsMap(seqsFile,chosedGeneHead);
-		System.out.println(seqsMap.get("6677"));
+		for(Map.Entry<String,String> entry:seqsMap.entrySet()) {
+			System.out.println(entry.getKey()+"="+entry.getValue());
+		}
 		System.out.println(seqsMap);
 		File codesFile=new File("C:\\Users\\Alleyne\\Documents\\javaStudy\\T.txt");
 		Map<String, String> codesMap=getCodesMap(codesFile);
@@ -30,19 +33,19 @@ public class FindGeneCode {
 	}
 	/*返回基因序列和编号的map*/
 	static Map<String,String> getSeqsMap(File seqsFile,String[] heads){
-		Map<String,String> rMap=new HashMap<>();
+		Map<String,String> rMap=new LinkedHashMap<>();
 		try(BufferedReader bfr=new BufferedReader(new FileReader(seqsFile))){
 			String str1=null;
 			String str2=null;
 			while((str1=bfr.readLine())!=null) {
 				str2=bfr.readLine();		
 				for(String head:heads) {
-					if(str2.substring(0, 3).equals(head)) {
+					if(str2.substring(0, 3).equals(head)) {//符合开头字符的才添加到map中
 						rMap.put(str1.substring(2),str2);//第一行截取编号，第二行是序列
 						break;
 					}
 				}
-				bfr.readLine();
+				bfr.readLine();//略去空白行
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -60,13 +63,19 @@ public class FindGeneCode {
 		String result=null;
 		try(BufferedReader br=new BufferedReader(new FileReader(geneFile))){
 			String str = null;
-			while((str=br.readLine())!=null) {geneSb.append(str);}
+			while((str=br.readLine())!=null) geneSb.append(str);
 			String gene=geneSb.toString();
-			seqsMap.forEach((k,v)->{
-				if(gene.indexOf(v)!=-1) {
-					geneCodeSb.append(k+"-");
+			TreeMap<Integer, String> tMap=new TreeMap<>();
+			seqsMap.forEach((k,v)->{//遍历map集合中基因序列，查找与基因文件匹配的序列，将对应编码
+				Integer i;			//及基因序列在基因文件中的索引放入TreeMap中,保证顺序
+				if((i=gene.indexOf(v))!=-1) {
+					tMap.put(i, k);
 				}
 			});
+			for(Entry entry:tMap.entrySet()) {
+				geneCodeSb.append(entry.getValue()+"-");
+			}
+			result=geneCodeSb.toString();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -84,7 +93,7 @@ public class FindGeneCode {
 	}
 	/*返回T文件标号与序列号的map*/
 	static Map<String, String> getCodesMap(File codesFile){
-		Map<String,String> tMap=new HashMap<>();
+		Map<String,String> tMap=new LinkedHashMap<>();
 		try(BufferedReader bfr=new BufferedReader(new FileReader(codesFile))){
 			String str=new String();
 			String[] strs=new String[2];
